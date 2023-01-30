3cbd72067e313e39b035e7590bb95db3b9dd88f2
Joakim Nordling <joakim.nordling@ioxio.com>
2023-01-30 14:26:28 +0200

    Add data products related to the SIX case (#36)

---
+++ src/draft/Product/Manufacturing/EnvironmentalFootprint.py
@@ -0,0 +1,33 @@
+from converter import CamelCaseModel, DataProductDefinition
+from pydantic import Field
+
+
+class EnvironmentalFootprintRequest(CamelCaseModel):
+    serial_number: str = Field(
+        ...,
+        title="Serial Number",
+        description="The serial number given by the manufacturer",
+        example="MPP48V-296cde7f",
+    )
+
+
+class EnvironmentalFootprintResponse(CamelCaseModel):
+    carbon_equivalent: float = Field(
+        ...,
+        title="Carbon Equivalent (CO2e) [kg]",
+        description="The amount of emissions from all greenhouse gases converted to CO2 emission equivalents in the product manufacturing phase",
+        example=200.0,
+    )
+    material_waste: float = Field(
+        ...,
+        title="Material Waste [kg]",
+        description="The amount of material waste produced in the product manufacturing phase",
+        example=8.0,
+    )
+
+
+DEFINITION = DataProductDefinition(
+    request=EnvironmentalFootprintRequest,
+    response=EnvironmentalFootprintResponse,
+    summary="Environmental Footprint information for a product",
+)
