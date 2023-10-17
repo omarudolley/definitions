f91e3888fcfd55b6626f36774c084d44bf5d13b7
omarudolley <58733599+omarudolley@users.noreply.github.com>
2023-10-17 13:43:50 +0300

    Updates the read and write data products for Person/BasicInformation and Person/JobApplicantProfile  (#62)
    * update person definitions
    
    * capitalize Date
    
    * Update src/Person/BasicInformation_v1.0.py
    
    Co-authored-by: Joakim Nordling <joakim.nordling@ioxio.com>
    
    * Update src/Person/BasicInformation_v1.0.py
    
    Co-authored-by: Joakim Nordling <joakim.nordling@ioxio.com>
    
    * Update src/Person/BasicInformation/Write_v1.0.py
    
    Co-authored-by: Joakim Nordling <joakim.nordling@ioxio.com>
    
    * Update src/Person/BasicInformation/Write_v1.0.py
    
    Co-authored-by: Joakim Nordling <joakim.nordling@ioxio.com>
    
    * version definitions
    
    ---------
    
    Co-authored-by: Joakim Nordling <joakim.nordling@ioxio.com>

---
+++ src/Person/BasicInformation_v2.0.py
@@ -0,0 +1,62 @@
+from enum import Enum
+from typing import Optional
+
+from definition_tooling.converter import CamelCaseModel, DataProductDefinition
+from pydantic import EmailStr, Field, constr
+
+
+class BasicInformationRequest(CamelCaseModel):
+    pass
+
+
+class CitizenshipArea(str, Enum):
+    EEA = "EEA"
+    NON_EEA = "non-EEA"
+
+
+class BasicInformationResponse(CamelCaseModel):
+    given_name: Optional[str] = Field(
+        None,
+        title="Given name",
+        description="The first name that the person is being called by",
+        example="John",
+        max_length=250,
+    )
+    last_name: Optional[str] = Field(
+        None,
+        title="Last name",
+        description="The person's current family name",
+        example="Doe",
+        max_length=250,
+    )
+    email: EmailStr = Field(
+        ...,
+        title="Email",
+        description="The person's contact email address",
+        example="john.doe@test.fi",
+    )
+    phone_number: Optional[str] = Field(
+        None,
+        title="Phone number",
+        description="The person's phone number in the international format",
+        example="+358501234567",
+        max_length=250,
+    )
+    citizenship_area: Optional[CitizenshipArea] = Field(
+        None,
+        title="Citizenship Area",
+        description="The citizenship area based on his or her native country. "
+        "Switzerland is considered as part of the EEA category.",
+        example=CitizenshipArea.EEA,
+    )
+
+
+DEFINITION = DataProductDefinition(
+    version="2.0.0",
+    title="Person Basic Information",
+    description="A minimal set of basic information of a person",
+    request=BasicInformationRequest,
+    response=BasicInformationResponse,
+    requires_authorization=True,
+    requires_consent=True,
+)
